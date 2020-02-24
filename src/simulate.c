4c0e0b692ace6465e165197a4aeba73fb595cbb8
Stefano Pozzi <spozzi@e-r9-p5.s19.be>
2020-02-24 14:23:16 +0100

    doing stuffx2

--- src/simulate.c
+++ src/simulate.c
@@ -49,45 +49,79 @@ void	sort_paths(t_struct *u)
 	}
 }
 
-void	send_waves(t_struct *u, int i)
+void	print_sol(t_struct *u, int n)
+{
+	int	i;
+	int j;
+
+	i = -1;
+	while (++i <= n)
+	{
+		j = 0;
+		while (u->paths[i][j] != u->snk)
+		{
+			j++;
+			if (u->hm->list[u->paths[i][j - 1]]->ant_ID != 0)
+			{
+				printf("(%d/%d): ", i, n);
+				printf("L%d-%s ", u->hm->list[u->paths[i][j - 1]]->ant_ID, u->hm->list[u->paths[i][j - 1]]->name);
+			}
+		}
+	}
+	printf("\n");
+}
+
+void	send_waves(t_struct *u, int i, int len[u->num_paths])
 {
 	int		ants_sent;
 	int		path_i;
 	int		j;
-	int		tmp;
 	int		ants_arrived;
-	int		first;
-	int		tmp1;
-	
+	int		flag;
+
 	ants_arrived = 0;
 	ants_sent = 0;
-	while (!ants_arrived)
+	flag = 0;
+	while (ants_arrived < u->ants)
 	{
 		path_i = -1;
 		while (++path_i <= i)
 		{
-			j = 0;
-			tmp = ++ants_sent;
-			first = 1;
-			while (u->paths[path_i][++j])
+			//printf("path_i: %d	i: %d	ants_arrived: %d\n", path_i, i, ants_arrived);
+			if (ants_sent < u->ants && !flag)
+				++ants_sent;
+			else
 			{
-				if (first)
-				{
-					u->hm->list[u->paths[path_i][j]]->ant_ID = tmp;
-					u->hm->list[u->paths[path_i][j]]->has_ant = 1;
-					first = 0;
-				}
-				else if (u->hm->list[u->paths[path_i][j]]->has_ant == 0)
-					u->hm->list[u->paths[path_i][j]]->ant_ID = 0;
-				else
-				{
-					tmp1 = u->hm->list[u->paths[path_i][j]]->ant_ID;
-					u->hm->list[u->paths[path_i][j]]->ant_ID = tmp;
-					tmp = tmp1;
-				}
-				if (ants_sent == u->ants)
-					ants_sent = 0;
+				++flag;
+				ants_sent = 0;
+				u->hm->list[u->src]->has_ant = 0;
 			}
+			j = 0;
+			while (u->paths[path_i][++j] != u->snk)
+				;
+			while (u->paths[path_i][j] != u->src)
+			{
+				if (ants_arrived == u->ants)
+					break;
+				if (u->paths[path_i][j] == u->snk
+						&& u->hm->list[u->paths[path_i][j - 1]]->has_ant == 1)
+					++ants_arrived;
+				if (u->paths[path_i][j - 1] == u->src && ants_sent != u->ants + 1)
+				{
+					u->hm->list[u->paths[path_i][j]]->ant_ID = ants_sent;
+					u->hm->list[u->paths[path_i][j]]->has_ant = 1;
+				}
+				if (u->hm->list[u->paths[path_i][j - 1]]->has_ant == 1)
+				{
+					u->hm->list[u->paths[path_i][j]]->ant_ID = u->hm->list[u->paths[path_i][j - 1]]->ant_ID;
+					u->hm->list[u->paths[path_i][j]]->has_ant = 1;
+				}
+				--j;
+			}
+		}
+		if (ants_arrived != u->ants)
+		{
+			print_sol(u, i);
 		}
 	}
 }
@@ -100,15 +134,30 @@ int		simulate(t_struct *u)
 	int		iter_curr;
 	int		num_gates_open;
 
+	int		len[u->num_paths];
 	int		sol_found;
 	int		i;
+	int		j;
 
 	sort_paths(u);
 	sol_found = 0;
 	i = -1;
 	while (++i < u->num_paths)
 	{
-		send_waves(u, i);
+		j = -1;
+		while (u->paths[i][++j] != u->snk)
+		{
+		 	u->hm->list[u->paths[i][j]]->ant_ID = 0;
+			u->hm->list[u->paths[i][j]]->has_ant = 0;
+		}
+		len[i] = j;
+	}
+	i = -1;
+	while (++i < u->num_paths)
+	{
+		send_waves(u, i, len);
+		printf("------------\n");
+
 	}
 	// u->max_paths = u->ants;
 	// max_iter = set_max_iter(u->num_paths);
